6bb28e88e26b63047c5f4de65d524679f4fca0b6
Francisco Jardelly <franciscojardelly@gmail.com>
2025-01-30 20:06:10 -0300

    Feat: add precommit

--- provider.tf
+++ provider.tf
@@ -2,11 +2,18 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.77.0"
+      version = "5.84.0"
     }
   }
+  backend "s3" {
+    bucket = "fjfs-infra"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
   # Configuration options
+  region = "us-east-1"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "eks_cluster" {
+resource "aws_vpc" "eks-vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "eks-cluster"
+    Name = "fjfs-eks-vpc"
   }
 }
