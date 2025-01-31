0ab24895afe849bd14a6ee7a3cd4b7d9bdae5bcc
Francisco Jardelly <franciscojardelly@gmail.com>
2025-01-30 22:24:48 -0300

    Feat: add subnet privada

--- sub-net-pub.tf
+++ sub-net-pub.tf
@@ -1,27 +1,29 @@
-resource "aws_subnet" "fjfs-subnet1a" {
-  vpc_id            = aws_vpc.eks-vpc.id
-  cidr_block        = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone = "${data.aws_region.current.name}a"
+resource "aws_subnet" "fjfs-subnet-pub-1a" {
+  vpc_id                  = aws_vpc.eks-vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
+  availability_zone       = "${data.aws_region.current.name}a"
+  map_public_ip_on_launch = true
 
   tags = merge(
     local.tags,
     {
-      Name                     = "fjfs-subnet1a",
+      Name                     = "fjfs-subnet-pub-1a",
       "kubernetes.io/role/elb" = 1
     }
   )
 }
 
 
-resource "aws_subnet" "fjfs-subnet1b" {
-  vpc_id            = aws_vpc.eks-vpc.id
-  cidr_block        = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone = "${data.aws_region.current.name}b"
+resource "aws_subnet" "fjfs-subnet-pub-1b" {
+  vpc_id                  = aws_vpc.eks-vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
+  availability_zone       = "${data.aws_region.current.name}b"
+  map_public_ip_on_launch = true
 
   tags = merge(
     local.tags,
     {
-      Name                     = "fjfs-subnet1b",
+      Name                     = "fjfs-subnet-pub-1b",
       "kubernetes.io/role/elb" = 1
     }
   )
