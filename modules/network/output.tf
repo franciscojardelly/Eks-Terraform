047060d742ee9743a6a496bcddc837d0e34ef223
Francisco Jardelly <franciscojardelly@gmail.com>
2025-02-09 13:22:39 -0300

    Feat: Add output network module

---
+++ modules/network/output.tf
@@ -0,0 +1,15 @@
+output "fjfs-subnet-pub-1a" {
+  value = aws_subnet.fjfs-subnet-pub-1a.id
+}
+
+output "fjfs-subnet-pub-1b" {
+  value = aws_subnet.fjfs-subnet-pub-1b.id
+}
+
+output "fjfs-subnet-priv-1a" {
+  value = aws_subnet.fjfs-subnet-priv-1a.id
+}
+
+output "fjfs-subnet-priv-1b" {
+  value = aws_subnet.fjfs-subnet-priv-1b.id
+}
